5c09acdcb87843da7c4de996f00e90abb05b25e9
George Hlongwane <georgehlongwane8@gmail.com>
2025-07-18 05:39:17 +0200

    Created views to handle form input and displaying test details

--- ab_test_planner/tests/views.py
+++ ab_test_planner/tests/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .forms import ABTestForm, ABTestVariantForm, ABTestOutcomeForm
+from .models import ABTest, ABTestVariant, ABTestOutcome
 
-# Create your views here.
+def home(request):
+    tests = ABTest.objects.all()
+    return render(request, 'tests/home.html', {'tests': tests})
+
+def create_test(request):
+    if request.method == 'POST':
+        form = ABTestForm(request.POST)
+        if form.is_valid():
+            test = form.save()
+            return redirect('view_test', test_id=test.id)
+    else:
+        form = ABTestForm()
+    return render(request, 'tests/create_test.html', {'form': form})
+
+def view_test(request, test_id):
+    test = get_object_or_404(ABTest, pk=test_id)
+    variant_form = ABTestVariantForm()
+    outcome_form = ABTestOutcomeForm()
+    return render(request, 'tests/view_test.html', {
+        'test': test,
+        'variant_form': variant_form,
+        'outcome_form': outcome_form,
+    })
+
+def add_variant(request, test_id):
+    test = get_object_or_404(ABTest, pk=test_id)
+    if request.method == 'POST':
+        form = ABTestVariantForm(request.POST)
+        if form.is_valid():
+            variant = form.save(commit=False)
+            variant.test = test
+            variant.save()
+            return redirect('view_test', test_id=test.id)
+    return redirect('view_test', test_id=test.id)
+
+def add_outcome(request, test_id):
+    test = get_object_or_404(ABTest, pk=test_id)
+    if request.method == 'POST':
+        form = ABTestOutcomeForm(request.POST)
+        if form.is_valid():
+            outcome = form.save(commit=False)
+            outcome.test = test
+            outcome.save()
+            return redirect('view_test', test_id=test.id)
+    return redirect('view_test', test_id=test.id)
